2e9175e1a19f6ff7d5fe298d81eb8c95f7b42cd6
Guntur Padilah <kangwe563@gmail.com>
2026-03-05 16:58:15 +0700

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
+/**
+ * @fileOverview Konfigurasi Firebase resmi More Ink.
+ * Pastikan authDomain menggunakan domain asli Firebase agar login stabil di Vercel.
+ */
 export const firebaseConfig = {
-  "projectId": "studio-3892278179-79044",
-  "appId": "1:977622653086:web:bc2efb1d7606776a34438f",
-  "apiKey": "AIzaSyAZdknTiV6OMQCy6XusMlHwjm-iiu7G-A4",
-  "authDomain": "studio-3892278179-79044.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "977622653086"
+  projectId: "studio-3892278179-79044",
+  appId: "1:977622653086:web:bc2efb1d7606776a34438f",
+  apiKey: "AIzaSyAZdknTiV6OMQCy6XusMlHwjm-iiu7G-A4",
+  authDomain: "studio-3892278179-79044.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "977622653086"
 };
