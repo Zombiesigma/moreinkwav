10bf97e5a8ff9ca44f5ab057b17b33d88b5cda6e
Guntur Padilah <kangwe563@gmail.com>
2026-03-05 16:38:08 +0700

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:977622653086:web:bc2efb1d7606776a34438f",
   "apiKey": "AIzaSyAZdknTiV6OMQCy6XusMlHwjm-iiu7G-A4",
   "authDomain": "moreink.vercel.app",
+  "authDomain": "studio-3892278179-79044.firebaseapp.com"
   "measurementId": "",
   "messagingSenderId": "977622653086"
 };
